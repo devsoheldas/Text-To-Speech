40dbbf77707a32c213813e70d0d8c2ba0b4808c2
Git Sohel <@devsoheldas>
2024-08-30 13:30:46 +0600

    Project Done

---
+++ app/src/main/java/com/techsohelbd/texttospeech/MainActivity.java
@@ -0,0 +1,49 @@
+package com.techsohelbd.texttospeech;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView edText;
+    Button button;
+    TextToSpeech textToSpeech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //variable initialize
+        edText = findViewById(R.id.edText);
+        button = findViewById(R.id.button);
+
+        textToSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                //code here
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //code here
+                textToSpeech.speak(edText.getText().toString(), TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+        });
+
+
+    }
+}
+//Coding by Developer Sohel
